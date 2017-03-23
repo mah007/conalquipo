1852505c5f762265b00171e0d2c965d67bf346eb
José Moreno <jmoreno@ias.com.co>
2017-03-23 17:21:12 -0500

    [CON]Adds con_profile module.

---
+++ con_profile/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Odoo CON Profile",
+
+    'summary': """
+        Conalequipos's Odoo Installation profile.
+        """,
+
+    'description': """
+        Conalequipo's Odoo Installation profile.
+    """,
+
+    'author': "Ingeniería Aplicaciones y Software",
+    'website': "http://www.ias.com.co",
+    'category': 'Base Profile',
+    'version': '0.1',
+    'depends': ['base', 'crm', 'project', 'odoo_outlook'],
+    'data': [
+        'views/outlook.xml',
+        'records/ir.model.access.csv',
+    ],
+    'qweb': [],
+    'images': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
