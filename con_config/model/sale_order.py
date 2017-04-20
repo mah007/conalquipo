8f00da4e4c36f8d46867ea8e4c0f85a417cafceb
sasprilla <shirly.asprilla@gmail.com>
2017-04-20 11:15:24 -0500

    [CON-001][IMP]Definir acarreo para el cliente

---
+++ con_config/model/sale_order.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+import logging
+
+from odoo import api, fields, models, _
+
+_logger = logging.getLogger(__name__)
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    carrier_type = fields.Selection(
+        [('client', 'Client'), ('company', 'Company')],
+        string='Carrier Responsible', default='fixed')
+
+    @api.onchange('carrier_type')
+    def onchange_carrier_type(self):
+        if self.carrier_type != 'company':
+            if self.partner_id:
+                # self._delivery_unset()
+                SaleOrderLine = self.env['sale.order.line']
+                res = SaleOrderLine.search([('order_id', '=', self._origin.id),
+                                            ('is_delivery', '=', True)])
+
+                res.unlink()
+                new_lines = SaleOrderLine.search([('order_id', '=',
+                                                    self._origin.id)])
+                for new in new_lines:
+                    self.write({'order_line': [
+                        (0, 0, {
+                                'order_id': self._origin.id,
+                                'name': new.name,
+                                'product_uom_qty': new.product_uom_qty,
+                                'product_uom': new.product_uom,
+                                'product_id': new.product_id,
+                                'price_unit': new.price_unit,
+                                'tax_id': new.tax_id,
+                                'is_delivery': False
+                        })]
+                    })
+                self.write({'carrier_id': None})
