b5651e164cb9518717804af31e11eacde43d11c6
José Moreno <jmoreno@ias.com.co>
2018-01-22 14:36:01 -0500

    [IMP] Clean Branch.

--- sale_customer_credit_limit/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# © 2016 Tobias Zehntner
-# © 2016 Niboo SPRL (https://www.niboo.be/)
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
-from . import partner
-from . import sale
-from . import company
