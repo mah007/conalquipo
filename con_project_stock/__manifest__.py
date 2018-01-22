6753ad56f4f2fbd8d3d6feafccc50a6a3c69289f
José Moreno <jmoreno@ias.com.co>
2018-01-22 16:53:00 -0500

    [IMP]Adds some fixes on con_project_stock.

---
+++ con_project_stock/__manifest__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Ingeniería, Aplicaciones y Software S.A.S
+#    Copyright (C) 2003-2017 Tiny SPRL (<http://www.ias.com.co>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': "CON Project in Stock",
+
+    'summary': """
+        Conalequipos's Project information for each customer on Stock form.
+    """,
+
+    'description': """
+        Conalequipos's Project information for each customer on Stock form.
+    """,
+
+    'author': "Ingeniería Aplicaciones y Software",
+    'website': "http://www.ias.com.co",
+    'category': 'stock',
+    'version': '0.1',
+    'depends': ['base', 'project', 'stock', 'con_project_works',
+                'con_client_code'],
+    'data': [
+        'views/stock_picking.xml',
+    ],
+    'qweb': [],
+    'images': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
