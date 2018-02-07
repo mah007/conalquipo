20e8630bacab4d75b4a233654af290d6d784aadb
José Moreno <jmoreno@ias.com.co>
2018-02-07 10:58:58 -0500

    [ADD]Adds new module con_purchase for now only the translations for this models have been added.

---
+++ con_purchase/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Conalquipo Purchase Customizations',
+    'version': '1.1',
+    'category': 'Purchase',
+    'sequence': 10,
+    'summary': 'Purchase, Stock',
+    'depends': ['base', 'stock', 'con_client_code', 'sale', 'con_sale',
+                'delivery'],
+    'description':
+    """
+        Customizations for conalquipo purchase process.
+    """,
+    'data': [
+    ],
+    'qweb': [
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
