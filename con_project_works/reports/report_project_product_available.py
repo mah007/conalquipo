4de0cc708e95edec3a35c5a2566c69385206a23f
Angel A. Guadarrama B <Angel A. Guadarrama B>
2018-03-22 16:42:35 -0500

    [UPD] - Modificacion del metodo get values del reporte

--- con_project_works/reports/report_project_product_available.py
+++ con_project_works/reports/report_project_product_available.py
@@ -7,21 +7,31 @@ import logging
 _logger = logging.getLogger(__name__)
 
 
-class report_project_product_available(models.AbstractModel):
+class ReportProjectProductAvailable(models.AbstractModel):
     _name = 'report.con_project_works.report_product_project'
 
     @api.model
     def get_report_values(self, docids, data=None):
         data = data if data is not None else {}
+        date_from = data['form']['date_from']
+        date_to = data['form']['date_to']
+        selectionby = data['form']['selectionby']
+        project_ids = self.env[
+            'project.project'].browse(
+                data.get('form', {}).get('project_ids', False))
+        partner_ids = self.env[
+            'res.partner'].browse(
+                data.get('form', {}).get('partner_ids', False))
+        move_line_ids = self.env[
+            'stock.move.line'].browse(
+                data.get('form', {}).get('move_line_ids', False))
         return {
             'doc_ids': data.get('ids', data.get('active_ids')),
-            'doc_model': 'project.project',
-            'data': dict(data),
-            'date_from': data['form']['date_from'],
-            'date_to': data['form']['date_to'],
-            'selectionby': data['form']['selectionby'],
-            'project_ids': self.env[
-                'project.project'].browse(data['form']['project_ids']),
-            'partners': self.env[
-                'res.partner'].browse(data['form']['partner_ids'])
-        }
+            'doc_model': 'stock.move.line',
+            'data': dict(data,
+                         date_from=date_from,
+                         date_to=date_to,
+                         selectionby=selectionby,
+                         project_ids=project_ids,
+                         partner_ids=partner_ids,
+                         move_line_ids=move_line_ids),}
--- con_project_works/wizards/models/project_product_available.py
+++ con_project_works/wizards/models/project_product_available.py
@@ -14,13 +14,60 @@ class ProjectProductAvailable(models.TransientModel):
         company = self.env['res.users'].browse([self._uid]).company_id
         return company
 
+    @api.onchange(
+        'selectionby',
+        'partner_ids',
+        'project_ids',
+        'date_from', 'date_to')
+    def onchange_picking(self):
+        projects_ids = []
+        partners_ids = []
+        moves_lines_ids = []
+        pickings_ids = []
+        if self.selectionby == 'partner':
+            projects = self.env[
+                'project.project'].search(
+                    [['partner_id', 'in', self.partner_ids._ids]])
+            for pro_data in projects:
+                pickings = self.env[
+                'stock.picking'].search(
+                    [['project_id', '=', pro_data.id],
+                     ['date', '>=', self.date_from],
+                     ['date', '<=', self.date_to],
+                     ['location_dest_id.usage',
+                       '=', 'customer']])
+                pickings_ids.append(pickings.id)
+                projects_ids.append(pro_data.id)
+            self.project_ids = projects_ids
+        else:
+            projects = self.env[
+                'project.project'].search(
+                    [['id', 'in', self.project_ids._ids]])
+            for part_data in projects:
+                pickings = self.env[
+                'stock.picking'].search(
+                    [['project_id', '=', part_data.id],
+                     ['date', '>=', self.date_from],
+                     ['date', '<=', self.date_to],
+                     ['location_dest_id.usage',
+                       '=', 'customer']])
+                pickings_ids.append(pickings.id)
+                partners_ids.append(part_data.partner_id.id)
+            self.partner_ids = partners_ids
+        moves = self.env[
+        'stock.move.line'].search(
+            [['picking_id', 'in', pickings_ids],
+             ['state', '=', 'done']])
+        for m in moves:
+            moves_lines_ids.append(m.id)
+        self.move_line_ids = moves_lines_ids
 
     company_id = fields.Many2one(
         'res.company', string="Company", required=True,
         default=lambda self: self._default_company())
-    date_from = fields.Date(
+    date_from = fields.Datetime(
         string="From")
-    date_to = fields.Date(
+    date_to = fields.Datetime(
         string="To")
     selectionby = fields.Selection(selection=[('partner', 'Partner'),
                                               ('project', 'Project')],
@@ -29,6 +76,8 @@ class ProjectProductAvailable(models.TransientModel):
                                   string="projects")
     partner_ids = fields.Many2many('res.partner',
                                   string="Partners")
+    move_line_ids = fields.Many2many(
+        'stock.move.line', string="Partners")
 
     @api.multi
     def print_report(self):
@@ -39,9 +88,11 @@ class ProjectProductAvailable(models.TransientModel):
         datas = {'ids': self.env.context.get('active_ids', [])}
         res = self.read(
             ['company_id', 'date_from', 'date_to',
-             'selectionby', 'project_ids', 'partner_ids'])
+             'selectionby', 'project_ids', 'partner_ids',
+             'move_line_ids'])
         res = res and res[0] or {}
         datas['form'] = res
         return self.env.ref(
             'con_project_works.action_report_product_project'
-        ).report_action([], data=datas)
+        ).with_context(
+            landscape=True).report_action([], data=datas)
