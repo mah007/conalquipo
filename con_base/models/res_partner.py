73913bb8ac811f3a075d5ba1ae3240b23fbf396d
Angel A. Guadarrama B <Angel A. Guadarrama B>
2018-05-17 10:25:56 -0500

    [ADD] - Se añade trazabilidad al campo tarifas en la ficha del cliente

--- con_base/models/res_partner.py
+++ con_base/models/res_partner.py
@@ -63,6 +63,18 @@ class ResPartnerCode(models.Model):
         column2='category_id',
         string='Tradename',
         default=_default_category)
+    property_product_pricelist = fields.Many2one(
+        'product.pricelist', 'Sale Pricelist', compute='_compute_product_pricelist',
+        inverse="_inverse_product_pricelist", company_dependent=False,  # NOT A REAL PROPERTY
+        help="This pricelist will be used, instead of the default one, for sales to the current partner",
+        track_visibility='onchange')
+
+    @api.multi
+    @api.depends('country_id')
+    def _compute_product_pricelist(self):
+        for p in self:
+            if not isinstance(p.id, models.NewId):  # if not onchange
+                p.property_product_pricelist = self.env['product.pricelist']._get_partner_pricelist(p.id)
 
     @api.model
     def create(self, values):
