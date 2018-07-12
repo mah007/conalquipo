74e2291439bf8127fb0047329bdee26f734a1b4a
Angel A. Guadarrama B <Angel A. Guadarrama B>
2018-07-12 11:14:58 -0500

    [UPD] - Cambiar la posición del campo de 'Referencia/Descripcion' en la pestaña "Otra información" a un lugar visible para el usuario. Cambio el nombre de factura rectificativa a nota de crédito.

--- con_profile/__manifest__.py
+++ con_profile/__manifest__.py
@@ -46,7 +46,7 @@
                 'con_product', 'con_project', 'con_project_translates',
                 'con_purchase_translates', 'con_sale','con_stock',
                 'con_web', 'con_website_sign',
-                'con_account', 'con_purchase', 
+                'con_account', 'con_purchase', 'con_account_translates',
     ],
     'data': [
         'data/res_lang_data.xml',
