997684738d9588b742db96e5b9ab3f85461cd73b
José Moreno <jmoreno@ias.com.co>
2018-09-05 17:40:54 -0500

    [FIX]Fixed errors on datetime format.

--- con_account/wizards/models/sale_advance_payment_inv.py
+++ con_account/wizards/models/sale_advance_payment_inv.py
@@ -6,7 +6,7 @@ from odoo import models, api, _, fields
 from odoo.exceptions import UserError
 from odoo.tools import float_is_zero, float_compare, \
     DEFAULT_SERVER_DATETIME_FORMAT
-from pytz import timezone
+from pytz import timezone, utc
 
 
 class SaleAdvancePaymentInv(models.TransientModel):
@@ -32,19 +32,26 @@ class SaleAdvancePaymentInv(models.TransientModel):
 
     @api.multi
     def create_invoices(self):
+        local = timezone(self._context['tz'])
         res = super(SaleAdvancePaymentInv, self).create_invoices()
+
         invoice = self.env['account.invoice'].search(
             [('id', '=', res['res_id'])])
-        date_init = datetime.strptime(
-            self.init_date_invoice,
+        date_init = datetime.strptime(self.init_date_invoice,
             DEFAULT_SERVER_DATETIME_FORMAT).replace(
                 hour=0, minute=0, second=0, microsecond=0)
-        date_end = datetime.strptime(
-            self.end_date_invoice,
-            DEFAULT_SERVER_DATETIME_FORMAT).replace(
-                hour=23, minute=59, second=59, microsecond=0)
+        local_dt_init = local.localize(date_init, is_dst=None)
+        utc_dt_init = local_dt_init.astimezone(utc)
+
+        _logger.info("Pass")
+        date_end = datetime.strptime(self.end_date_invoice,
+             DEFAULT_SERVER_DATETIME_FORMAT).replace(
+                 hour=23, minute=59, second=59, microsecond=0)
+        local_dt_end = local.localize(date_end, is_dst=None)
+        utc_dt_end = local_dt_end.astimezone(utc)
+
         if invoice.invoice_type == 'rent':
             invoice.write({
-                'init_date_invoice': date_init,
-                'end_date_invoice': date_end})
+                'init_date_invoice': utc_dt_init,
+                'end_date_invoice': utc_dt_end})
         return res
