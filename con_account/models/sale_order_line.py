304ad695f1bca5f7f1ddab84053cc17796aaaf5e
Edgar Rivero <edgarrivero@gmail.com>
2018-09-20 20:45:30 +0000

    [IMP] con_account: No se permiten solapar fechas de facturacion.

--- con_account/models/sale_order_line.py
+++ con_account/models/sale_order_line.py
@@ -51,6 +51,33 @@ class SaleOrder(models.Model):
 
         return invoice_vals
 
+    @api.multi
+    def action_invoice_create(self, grouped=False, final=False):
+        """
+        Create the invoice associated to the SO.
+        :param grouped: if True, invoices are grouped by SO id.
+            If False, invoices are grouped by
+            (partner_invoice_id, currency)
+        :param final: if True, refunds will be generated if necessary
+        :returns: list of created invoices
+        """
+        if self.env['account.invoice'].search(
+                ['|', '|', '&',
+                 ('init_date_invoice', '<=', self.init_date_invoice),
+                 ('end_date_invoice', '>=', self.init_date_invoice),
+                 '&',
+                 ('init_date_invoice', '<=', self.end_date_invoice),
+                 ('end_date_invoice', '>=', self.end_date_invoice),
+                 '&',
+                 ('init_date_invoice', '>=', self.init_date_invoice),
+                 ('end_date_invoice', '<=', self.end_date_invoice),
+                 ('partner_id', '=', self.partner_id.id),
+                 ('project_id', '=', self.project_id.id)]):
+            raise UserError(
+                'You are trying to create an invoice in a period and invoice.')
+        values = super(SaleOrder, self).action_invoice_create(grouped, final)
+        return values
+
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
